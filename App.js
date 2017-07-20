fe7b292c0874fb0d91bd11e140b7d2b8fe926105
Marissa Monivis <mmonivis@gmail.com>
2017-07-19 20:34:00 -0400

    Init commit

---
+++ App.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this)
+    this.state = {
+      amount: 0
+    }
+  }
+
+  handleChange(e){
+    console.log(e)
+    this.setState({
+      amount: e
+    })
+  }
+
+  render() {
+    // const goodTip = Math.round(this.state.amount * .2 * 100) / 100
+    const goodTip = parseFloat(this.state.amount * .2).toFixed(2)
+    const okTip = parseFloat(this.state.amount * .1).toFixed(2)
+    return (
+      <View style={styles.container}>
+        <View style={styles.nav}>
+          <TextInput
+            style={styles.amount}
+            placeholder="Enter Bill Amount"
+            placeholderTextColor="#9A73EF"
+            keyboardType = "numeric"
+            onChangeText = {this.handleChange}
+          />
+        </View>
+        <View>
+          <Text>${goodTip} - for great service</Text>
+          <Text>${okTip} - for OK service</Text>
+
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ff0',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  amount: {
+    margin: 20,
+    height: 40,
+    width: '80%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    padding: 10
+  }
+});
